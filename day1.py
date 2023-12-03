d66e81b710e9f96c5b2003681b2ae5e26048f624
Emi Goh <emigoh@adrians-mbp.lan>
2023-12-02 19:44:11 -0500

    solutionin a different way, part 1

--- day1.py
+++ day1.py
@@ -8,20 +8,24 @@ def main():
     number = list_to_number(sanitizedInput)
     print(number)
 
+# def list_to_number(string_list):
+#     i=0
+#     sum=0
+#     while i < len(string_list):
+#         num_text = string_list[i][0] + string_list[i][len(string_list[i])-1]
+#         num = int(num_text)
+#         sum+= num
+#         i+=1
+
+#     return sum
+
 def list_to_number(string_list):
-    i=0
-    sum=0
-    while i < len(string_list):
-        num_text = string_list[i][0] + string_list[i][len(string_list[i])-1]
+    sum = 0
+    for val in string_list:
+        num_text = val[0] + val[len(val)-1]
         num = int(num_text)
         sum+= num
-        i+=1
-
     return sum
-
-
-
-
 
 
 main()
